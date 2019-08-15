72440524df4c3df232c7b262ee476760e412d8fc
Johan <johan.sinaga@hotmail.com>
2019-08-15 20:54:28 +0200

    Updated the output to be colored Red or Green based on the increase or decrease in % value

--- getLastMonthPerformance.py
+++ getLastMonthPerformance.py
@@ -1,19 +1,26 @@
 import sys
 from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
+from colorama import init
+from colorama import Fore, Back, Style
 
+# Initialize colorama for coloring
+init()
 
+# Inserting shares TODO: use config file?
 shares = [	'ACB', 'PDD', 'YUMC', 'ACN', 'PEP',
 			'DSM.AS', 'LIGHT.AS', 'RDSA.AS', 'T', 'BABA',
 			'ABN.AS', 'VWRL.AS', 'NN.AS', 'SPOT', 'AAPL'
 			]
 
-print(('TICKER\t\t\tLast Month Price\tYesterday Price\t\tChange in %'))
+# Print header
+print(('\033[1;37;40mTICKER\t\tLast Month Price     Yesterday Price\tChange in %'))
 
 for my_share in shares:
 	my_share_date = share.Share(my_share)
 	symbol_data = None
-
+	
+	# Getting data from Yahoo Finance
 	try:
 		symbol_data = my_share_date.get_historical(share.PERIOD_TYPE_MONTH,
 											  1,
@@ -23,19 +30,18 @@ for my_share in shares:
 		print("Cannot find data for ticker symbol", my_share)
 		print(e.message)
 		sys.exit(1)
-
+		
+	# Calculation
 	lastMonthClosePrice = float(symbol_data['close'][0])
 	yesterdayClosePrice = float(symbol_data['close'][-2])
 	ChangePercent = float(yesterdayClosePrice - lastMonthClosePrice)
 	
-	'''
-	print ("Share: ", my_share)
-	print ("")
-	print ("Last month price:\t {:.2f}".format(lastMonthClosePrice))
-	print ("Yesterday price:\t {:.2f}".format(yesterdayClosePrice))
-	print ("Change:\t\t\t {:.2f}".format(ChangePercent), "%")
-	print ("")
-	print ("")
-	'''
 	
-	print('{:<20}\t{:5.2f}\t\t\t{:6.2f}\t\t\t{:2.2f}%'.format(my_share, lastMonthClosePrice, yesterdayClosePrice, ChangePercent, ChangePercent))
+	
+	# Print output	
+	if yesterdayClosePrice < lastMonthClosePrice:		# print in red
+		print('\033[1;37;40m{:<20}{:6.2f}\t\t{:6.2f}\t\t\033[1;31;40m{:6.2f}%'.format
+		(my_share, lastMonthClosePrice, yesterdayClosePrice, ChangePercent, ChangePercent))
+	else:												# Print in green
+		print('\033[1;37;40m{:<20}{:6.2f}\t\t{:6.2f}\t\t\033[1;32;40m{:6.2f}%'.format
+		(my_share, lastMonthClosePrice, yesterdayClosePrice, ChangePercent, ChangePercent))
