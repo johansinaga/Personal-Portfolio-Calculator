d8584f21d1046167a4ecf9e1fcbc96ce7cd3ac06
Johan <johan.sinaga@hotmail.com>
2019-08-15 23:25:50 +0200

    Added last 2w and 1w change, clean up look

--- getLastMonthPerformance.py
+++ getLastMonthPerformance.py
@@ -2,19 +2,35 @@ import sys
 from yahoo_finance_api2 import share
 from yahoo_finance_api2.exceptions import YahooFinanceError
 from colorama import init
-from colorama import Fore, Back, Style
+
+formatColorWhite = '\033[1;37;40m'
+formatColorRed = '\033[1;31;40m'
+formatColorGreen = '\033[1;32;40m'
+
+index2weeks = -11
+index1weeks = -6
+indexYesterday = -2
+indexToday = -1
+
+def correctPrice(price):
+	if (price == None):
+		newIndex = index2weeks -1
+		while (price == None):
+			price = symbol_data['close'][newIndex]
+			newIndex = newIndex -1
+	return float(price)
 
 # Initialize colorama for coloring
 init()
 
-# Inserting shares TODO: use config file?
+# Inserting shares			//TODO: use config file?
 shares = [	'ACB', 'PDD', 'YUMC', 'ACN', 'PEP',
 			'DSM.AS', 'LIGHT.AS', 'RDSA.AS', 'T', 'BABA',
 			'ABN.AS', 'VWRL.AS', 'NN.AS', 'SPOT', 'AAPL'
 			]
 
 # Print header
-print(('\033[1;37;40mTICKER\t\tLast Month Price     Yesterday Price\tChange in %'))
+print(('\033[1;37;40mTICKER\t   Last Month Price   Yesterday Price\t1m Change %\t2w Change %\t1w Change %'))
 
 for my_share in shares:
 	my_share_date = share.Share(my_share)
@@ -32,14 +48,43 @@ for my_share in shares:
 		sys.exit(1)
 		
 	# Calculation
-	lastMonthClosePrice = float(symbol_data['close'][0])
-	yesterdayClosePrice = float(symbol_data['close'][-2])
-	ChangePercent = float((yesterdayClosePrice - lastMonthClosePrice)/lastMonthClosePrice*100)
+	lastMonthClosePrice = symbol_data['close'][0]
+	_2WeeksAgoPrice = symbol_data['close'][index2weeks]
+	_1WeeksAgoPrice = symbol_data['close'][index1weeks]
+	yesterdayClosePrice = symbol_data['close'][indexYesterday]	
 	
-	# Print output	
-	if yesterdayClosePrice < lastMonthClosePrice:		# print in red
-		print('\033[1;37;40m{:<20}{:6.2f}\t\t{:6.2f}\t\t\033[1;31;40m{:6.2f}%'.format
-		(my_share, lastMonthClosePrice, yesterdayClosePrice, ChangePercent, ChangePercent))
-	else:												# Print in green
-		print('\033[1;37;40m{:<20}{:6.2f}\t\t{:6.2f}\t\t\033[1;32;40m{:6.2f}%'.format
-		(my_share, lastMonthClosePrice, yesterdayClosePrice, ChangePercent, ChangePercent))
+	
+	################################################################	Handling if no price history can be found at certain date using a function
+	lastMonthClosePrice = correctPrice(lastMonthClosePrice)
+	_2WeeksAgoPrice = correctPrice(_2WeeksAgoPrice)
+	_1WeeksAgoPrice = correctPrice(_1WeeksAgoPrice)
+	yesterdayClosePrice = correctPrice(yesterdayClosePrice)
+	################################################################
+	
+
+	_1MonthChangePercent = float((yesterdayClosePrice - lastMonthClosePrice)/lastMonthClosePrice*100)
+	_2WeeksChangePercent = float((yesterdayClosePrice - _2WeeksAgoPrice)/_2WeeksAgoPrice*100)
+	_1WeeksChangePercent = float((yesterdayClosePrice - _1WeeksAgoPrice)/_1WeeksAgoPrice*100)
+
+	todayPrice = float(symbol_data['close'][indexToday])
+	
+	
+	# Determine Color Format based on %
+	if (_1MonthChangePercent < 0):
+		formatColor1m = formatColorRed
+	else:
+		formatColor1m = formatColorGreen
+	if (_2WeeksChangePercent < 0):
+		formatColor2w = formatColorRed
+	else:
+		formatColor2w = formatColorGreen
+	if (_1WeeksChangePercent < 0):
+		formatColor1w = formatColorRed
+	else:
+		formatColor1w = formatColorGreen
+	
+	
+	# Print output
+	print('{}{:<15}{:6.2f}\t\t{:6.2f}\t\t{}{:6.2f}\t\t{}{:6.2f}\t\t{}{:6.2f}{}'.format
+		(formatColorWhite, my_share, lastMonthClosePrice, yesterdayClosePrice, formatColor1m, _1MonthChangePercent,
+		formatColor2w, _2WeeksChangePercent, formatColor1w, _1WeeksChangePercent, formatColorWhite))
